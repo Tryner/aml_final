2a60547598c72f9d863c25417c4dd8b639315404
Tryner <spamisttoll@web.de>
2023-11-29 19:40:32 +0100

    finish basic loop

--- train/active_learning.py
+++ train/active_learning.py
@@ -1,17 +1,32 @@
+from typing import Callable
+
 from datasets import Dataset, DatasetDict
 from setfit import SetFitModel, SetFitTrainer
 
 from .train_config import TrainConfig
 from .active_learning_config import ActiveLearningConfig
 
-def active_train(model: SetFitModel, dataset: Dataset, train_config: TrainConfig, active_learning_config: ActiveLearningConfig):
-    pass
+def active_train(model_init: Callable[[], SetFitModel], dataset: DatasetDict, train_config: TrainConfig, active_learning_config: ActiveLearningConfig):
+    train_dataset = dataset["train"]
+    eval_dataset = dataset["validation"]
+    subset = train_dataset #TODO
+    trainer = run_training(model_init=model_init, train_dataset=subset, eval_dataset=eval_dataset, train_config=train_config)
+    for _ in range(active_learning_config.active_learning_cycles):
+        subset = subset #TODO add examples
+        trainer = run_training(model_init=model_init, train_dataset=subset, eval_dataset=eval_dataset, train_config=train_config)
+    return trainer
 
 
-def run_training(model: SetFitModel, train_dataset: Dataset, train_config: TrainConfig):
+def run_training(
+        model_init: Callable[[], SetFitModel], 
+        train_dataset: Dataset, 
+        eval_dataset: Dataset, 
+        train_config: TrainConfig
+        ) -> SetFitTrainer:
     trainer = SetFitTrainer(
-        model=model,
+        model_init=model_init,
         train_dataset=train_dataset,
+        eval_dataset=eval_dataset,
         loss_class=train_config.loss_class,
         metric=train_config.metric,
         batch_size=train_config.batch_size,
@@ -32,7 +47,7 @@ def run_training(model: SetFitModel, train_dataset: Dataset, train_config: Train
         learning_rate=1e-2,
         l2_weight=0.0,
     ) # add to TrainConfig
-    return trainer.model
+    return trainer
 
 
 
