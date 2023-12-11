9f1ec9dcddc7ad8cccdfb2214c4890721ce33c2b
GiorgioSV99 <118373082+GiorgioSV99@users.noreply.github.com>
2023-12-11 17:23:28 +0100

    Update load_datasets.py

--- data/load_datasets.py
+++ data/load_datasets.py
@@ -42,3 +42,23 @@ def load_spanish_dataset(name):
     vset = Dataset.from_pandas(pd.concat([X_valid, y_valid], axis=1))
     dataset_dict = DatasetDict({'train': tset, 'validation': vset})
     return dataset_dict
+
+
+
+def load_indonesian_dataset(name):
+
+    df=pd.read_csv(f'data/raw_datasets/{name}.csv')
+
+    df=df.loc[:,['text','label']]
+    df=df.rename(columns={'text': DatasetConfig.text_column, 'label': DatasetConfig.label_column})
+    X = df.drop('label', axis=1)  # Assuming 'label' is the name of your label column
+    y = df['label']
+
+    # Split the data into training and validation sets
+    X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    # Create DataFrames for training and validation
+    tset = Dataset.from_pandas(pd.concat([X_train, y_train], axis=1))
+    vset = Dataset.from_pandas(pd.concat([X_valid, y_valid], axis=1))
+    dataset_dict = DatasetDict({'train': tset, 'validation': vset})
+    return dataset_dict
