d367cb7b9065cdf6f6b81f5aaa5eb3a0f4a143c6
GiorgioSV99 <118373082+GiorgioSV99@users.noreply.github.com>
2023-12-11 17:37:21 +0100

    Update load_datasets.py

--- data/load_datasets.py
+++ data/load_datasets.py
@@ -45,20 +45,26 @@ def load_spanish_dataset(name):
 
 
 
-def load_indonesian_dataset(name):
+def load_indonesian_dataset():
 
-    df=pd.read_csv(f'data/raw_datasets/{name}.csv')
+    dataset_path = "/content/drive/MyDrive/aml_final-main/aml_final-main/data/raw_datasets/datd_train.csv"
+    #dataset = load_dataset("csv", data_files=dataset_path)
+    from sklearn.model_selection import train_test_split
+    from datasets import load_dataset
 
-    df=df.loc[:,['text','label']]
-    df=df.rename(columns={'text': DatasetConfig.text_column, 'label': DatasetConfig.label_column})
-    X = df.drop('label', axis=1)  # Assuming 'label' is the name of your label column
-    y = df['label']
+    # Carica il tuo dataset CSV
+    dataset = load_dataset("csv", data_files=dataset_path )["train"]
 
-    # Split the data into training and validation sets
-    X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=0.2, random_state=42)
+    # Specifica la proporzione di addestramento e di validazione
+    train_ratio = 0.75
+    validation_ratio = 0.25
 
-    # Create DataFrames for training and validation
-    tset = Dataset.from_pandas(pd.concat([X_train, y_train], axis=1))
-    vset = Dataset.from_pandas(pd.concat([X_valid, y_valid], axis=1))
-    dataset_dict = DatasetDict({'train': tset, 'validation': vset})
-    return dataset_dict
+    # Calcola le dimensioni dei set di addestramento e validazione
+    train_size = int(len(dataset) * train_ratio)
+    validation_size = len(dataset) - train_size
+
+    # Fai lo split del dataset
+    train_dataset, validation_dataset = train_test_split(dataset, test_size=validation_ratio, random_state=42)
+
+    # Ora puoi utilizzare train_dataset e validation_dataset per l'addestramento e la validazione
+    return {"train":Dataset.from_dict(train_dataset),"validation": Dataset.from_dict(validation_dataset)}
