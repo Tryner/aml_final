a343bb7cb7b8ccc38635bac89fc3dfd96c73d02c
Tryner <spamisttoll@web.de>
2023-12-14 17:19:45 +0100

    use spanish twitter dataset

--- data/load_datasets.py
+++ data/load_datasets.py
@@ -53,14 +53,29 @@ def load_french_dataset(name):
     dataset_dict = DatasetDict({'train': tset, 'validation': vset})
     return dataset_dict
 
+def label_row(row):
+    if row["prejudice_woman"] == 1:
+        return 0
+    if row["prejudice_lgbtiq"] == 1:
+        return 1
+    if row["prejudice_inmigrant_race"] == 1:
+        return 2
+    if row["gordofobia"] == 1:
+        return 3
+    raise ValueError("label missing!")
 
 def load_spanish_dataset(name):
     df=pd.read_csv(f'data/raw_datasets/{name}.csv')
 
-    df=df.loc[:,['tweet','humor']]
-    df=df.rename(columns={'tweet': DatasetConfig.text_column, 'humor': DatasetConfig.label_column})
-    X = df.drop('label', axis=1)  # Assuming 'label' is the name of your label column
-    y = df['label']
+    del df["index"]
+    del df["mean_prejudice"]
+    df["sum"] = df.loc[:, ['prejudice_woman', 'prejudice_lgbtiq', 'prejudice_inmigrant_race', 'gordofobia']].sum(axis=1)
+    df = df.loc[df['sum'] == 1]
+    del df["sum"]
+    df["label"] = [label_row(row) for _, row in df.iterrows()]
+
+    X = df['tweet']
+    y = df.drop('tweet', axis=1)
 
     # Split the data into training and validation sets
     X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=0.2, random_state=42)
