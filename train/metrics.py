9136557a1497a0d8cf96acb45e7e138edcfaadb1
Tryner <spamisttoll@web.de>
2023-12-26 23:23:58 +0100

    streamline basics

--- train/metrics.py
+++ train/metrics.py
@@ -1,7 +1,7 @@
 from sklearn import metrics
 from datasets import Dataset
 
-def camprehesive_metrics(y_pred: Dataset, y_true: Dataset, **metric_kwargs) -> dict[str, float]:
+def comprehensive_metrics(y_pred: Dataset, y_true: Dataset, **metric_kwargs) -> dict[str, float]:
     accuracy = metrics.accuracy_score(y_true=y_true, y_pred=y_pred)
     balanced_accuracy = metrics.balanced_accuracy_score(y_true=y_true, y_pred=y_pred)
     results = {
